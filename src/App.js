a7ecc3d033f1d599e320e90c138725ba7ba5453f
Admin <you@example.com>
2022-02-09 19:03:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import image from './html-css-js.jpg';
+import { Row } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <div className='p' style={{border:"solid 10px black" ,Width:"500px",height:"800px" ,textAlign:"center"   }}>
+
+ <h1  className="title red">Your name here</h1>
+
+ 
+
+ <img width="320" height="240" src= {image}/>
+
+ 
+
+ <img width="320" height="240"src="./x.png" />
+
+
+ <video width="320" height="240" controls>
+
+ <source src="myVideo.mp4" type="video/mp4" />
+
+</video>
+
+</div>
+</>
   );
 }
 
